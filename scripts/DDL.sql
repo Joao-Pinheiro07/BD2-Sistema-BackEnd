67e7a82f87508442bfa9da294c8fd10063aab0b1
João Matheus Pinheiro <joaomatheuspinheiro@gmail.com>
2021-07-18 12:13:44 -0300

    add DDL and DML

--- scripts/DDL.sql
+++ scripts/DDL.sql
@@ -8,12 +8,12 @@
 /* Table: ENDERECO                                              */
 /*==============================================================*/
 create table ENDERECO (
-   ENDERECO_ID          INT2                 not null,
-   RUA                  VARCHAR(60)          not null,
-   BAIRRO               VARCHAR(60)          not null,
-   CIDADE               VARCHAR(20)          not null,
-   ESTADO               VARCHAR(20)          not null,
-   CEP                  VARCHAR(9)           not null,
+   ENDERECO_ID          SERIAL                 not null,
+   RUA                  VARCHAR(50)          not null,
+   BAIRRO               VARCHAR(50)          not null,
+   CIDADE               VARCHAR(50)          not null,
+   ESTADO               VARCHAR(50)          not null,
+   CEP                  VARCHAR(50)           not null,
    constraint PK_ENDERECO primary key (ENDERECO_ID)
 );
 
@@ -21,11 +21,11 @@ create table ENDERECO (
 /* Table: GERENTE                                               */
 /*==============================================================*/
 create table GERENTE (
-   GERENTE_ID           INT2                not null,
-   EMAIL                VARCHAR(20)          not null,
-   CPF_GERENTE          VARCHAR(11)          null,
-   NOME_GER             VARCHAR(25)          null,
-   SOBRENOME_GER        VARCHAR(25)          null,
+   GERENTE_ID           SERIAL                not null,
+   EMAIL                VARCHAR(50)          not null,
+   CPF_GERENTE          VARCHAR(50)          null,
+   NOME_GER             VARCHAR(50)          null,
+   SOBRENOME_GER        VARCHAR(50)          null,
    constraint PK_GERENTE primary key (GERENTE_ID)
 );
 
@@ -33,10 +33,10 @@ create table GERENTE (
 /* Table: FRANQUIA                                              */
 /*==============================================================*/
 create table FRANQUIA (
-   FRANQUIA_ID          INT2                not null,
-   GERENTE_ID           INT2                 not null,
-   ENDERECO_ID          INT2                 not null,
-   UNIDADE_FRANQUIA     VARCHAR(15)          not null,
+   FRANQUIA_ID          SERIAL                not null,
+   GERENTE_ID           INTEGER                 not null,
+   ENDERECO_ID          INTEGER                 not null,
+   UNIDADE_FRANQUIA     VARCHAR(50)          not null,
    constraint PK_FRANQUIA primary key (FRANQUIA_ID),
    constraint FK_FRANQUIA_R10_ENDERECO foreign key (ENDERECO_ID)
       references ENDERECO (ENDERECO_ID)
@@ -46,29 +46,17 @@ create table FRANQUIA (
       on delete restrict on update restrict
 );
 
-/*==============================================================*/
-/* Table: CAIXA                                                 */
-/*==============================================================*/
-create table CAIXA (
-   CAIXA_ID             INT2                not null,
-   FRANQUIA_ID          INT2                 not null,
-   NUMERO               INT2                 not null,
-   constraint PK_CAIXA primary key (CAIXA_ID),
-   constraint FK_CAIXA_R20_FRANQUIA foreign key (FRANQUIA_ID)
-      references FRANQUIA (FRANQUIA_ID)
-      on delete restrict on update restrict
-);
 
 /*==============================================================*/
 /* Table: CARRO                                                 */
 /*==============================================================*/
 create table CARRO (
-   CARRO_ID             INT2                not null,
-   FRANQUIA_ID          INT2                 not null,
-   PLACA                VARCHAR(10)          not null,
-   MODELO               VARCHAR(20)          not null,
-   ANO                  VARCHAR(4)           not null,
-   MARCA_CARRO          VARCHAR(20)          null,
+   CARRO_ID             SERIAL                not null,
+   FRANQUIA_ID          INTEGER                 not null,
+   PLACA                VARCHAR(50)          not null,
+   MODELO               VARCHAR(50)          not null,
+   ANO                  VARCHAR(50)           not null,
+   MARCA_CARRO          VARCHAR(50)          null,
    constraint PK_CARRO primary key (CARRO_ID),
    constraint FK_CARRO_RELATIONS_FRANQUIA foreign key (FRANQUIA_ID)
       references FRANQUIA (FRANQUIA_ID)
@@ -79,11 +67,11 @@ create table CARRO (
 /* Table: CLIENTE                                               */
 /*==============================================================*/
 create table CLIENTE (
-   CLIENTE_ID           INT2               not null,
-   CPF_CLIENTE          VARCHAR(11)          not null,
-   CELULAR              VARCHAR(11)          not null,
-   ENDERECO_ID          INT2                 not null,
-   NOME_CLI             VARCHAR(25)          null,
+   CLIENTE_ID           SERIAL               not null,
+   CPF_CLIENTE          VARCHAR(50)          not null,
+   CELULAR              VARCHAR(50)          not null,
+   ENDERECO_ID          INTEGER              not null,
+   NOME_CLI             VARCHAR(50)          null,
    SOBRENOME_CLI        VARCHAR(50)          null,
    constraint PK_CLIENTE primary key (CLIENTE_ID),
    constraint FK_CLIENTE_R13_ENDERECO foreign key (ENDERECO_ID)
@@ -95,17 +83,16 @@ create table CLIENTE (
 /* Table: FUNCIONARIO                                           */
 /*==============================================================*/
 create table FUNCIONARIO (
-   NOME_FUNC            VARCHAR(15)          not null,
-   SOBRENOME_FUNC       VARCHAR(20)          not null,
-   CARGO                VARCHAR(15)          not null,
-   FUNCIONARIO_ID       INT2                not null,
-   FUN_FUNCIONARIO_ID   INT2                 null,
-   FRANQUIA_ID          INT2                 not null,
+   NOME_FUNC            VARCHAR(50)          not null,
+   SOBRENOME_FUNC       VARCHAR(50)          not null,
+   CARGO                VARCHAR(50)          not null,
+   FUNCIONARIO_ID       SERIAL               not null,
+   SUPERVISOR_ID        INTEGER                  null,
+   FRANQUIA_ID          INTEGER              not null,
    DATA_ADMISSAO        DATE                 not null,
-   CPF_FUNC             VARCHAR(11)          null,
-   SALARIO              MONEY                null,
+   CPF_FUNC             VARCHAR(50)          null,
    constraint PK_FUNCIONARIO primary key (FUNCIONARIO_ID),
-   constraint FK_FUNCIONA_RELATIONS_FUNCIONA foreign key (FUN_FUNCIONARIO_ID)
+   constraint FK_FUNCIONA_RELATIONS_FUNCIONA foreign key (SUPERVISOR_ID)
       references FUNCIONARIO (FUNCIONARIO_ID)
       on delete restrict on update restrict,
    constraint FK_FUNCIONA_RELATIONS_FRANQUIA foreign key (FRANQUIA_ID)
@@ -113,22 +100,28 @@ create table FUNCIONARIO (
       on delete restrict on update restrict
 );
 
+create table CLT (
+   FUNCIONARIO_ID       INTEGER               not null,
+   SALARIO              MONEY                not null,
+   constraint PK_CLT primary key (FUNCIONARIO_ID),
+   constraint FK_CLT foreign key (FUNCIONARIO_ID)
+      references FUNCIONARIO (FUNCIONARIO_ID)
+      on delete restrict on update restrict
+);
+
 /*==============================================================*/
 /* Table: VENDA                                                 */
 /*==============================================================*/
 create table VENDA (
-   VENDA_ID             INT2                 not null,
-   FUNCIONARIO_ID       INT2                 not null,
-   FRANQUIA_ID          INT2                 not null,
-   CLIENTE_ID           INT2                 not null,
-   CAIXA_ID             INT2                 not null,
+   VENDA_ID             SERIAL               not null,
+   FUNCIONARIO_ID       INTEGER              not null,
+   FRANQUIA_ID          INTEGER              not null,
+   CLIENTE_ID           INTEGER              not null,
+   CAIXA             INTEGER              not null,
    VALOR                MONEY                not null,
    DATA_VENDA           DATE                 not null,
-   PARCELAS             FLOAT4               not null,
+   PARCELAS             smallint               not null,
    constraint PK_VENDA primary key (VENDA_ID),
-   constraint FK_VENDA_R8_CAIXA foreign key (CAIXA_ID)
-      references CAIXA (CAIXA_ID)
-      on delete restrict on update restrict,
    constraint FK_VENDA_R2_CLIENTE foreign key (CLIENTE_ID)
       references CLIENTE (CLIENTE_ID)
       on delete restrict on update restrict,
@@ -136,7 +129,7 @@ create table VENDA (
       references FRANQUIA (FRANQUIA_ID)
       on delete restrict on update restrict,
    constraint FK_VENDA_R19_FUNCIONA foreign key (FUNCIONARIO_ID)
-      references FUNCIONARIO (FUNCIONARIO_ID)
+      references CLT (FUNCIONARIO_ID)
       on delete restrict on update restrict
 );
 
@@ -144,14 +137,7 @@ create table VENDA (
 /* Table: TERCEIRO                                              */
 /*==============================================================*/
 create table TERCEIRO (
-   FUNCIONARIO_ID       INT2                 not null,
-   NOME_FUNC            VARCHAR(15)          not null,
-   SOBRENOME_FUNC       VARCHAR(20)          not null,
-   CARGO                VARCHAR(15)          not null,
-   FUN_FUNCIONARIO_ID   INT2                 null,
-   FRANQUIA_ID          INT2                 null,
-   DATA_ADMISSAO        DATE                 not null,
-   CPF_FUNC             VARCHAR(11)          null,
+   FUNCIONARIO_ID       INTEGER               not null,
    NOME_EMPRESA         VARCHAR(50)          not null,
    constraint PK_TERCEIRO primary key (FUNCIONARIO_ID),
    constraint FK_TERCEIRO_INHERITAN_FUNCIONA foreign key (FUNCIONARIO_ID)
@@ -163,11 +149,11 @@ create table TERCEIRO (
 /* Table: ENTREGA                                               */
 /*==============================================================*/
 create table ENTREGA (
-   ENTREGA_ID           INT2                 not null,
-   CARRO_ID             INT2                 not null,
-   VENDA_ID             INT2                 not null,
-   ENDERECO_ID          INT2                 not null,
-   FUNCIONARIO_ID       INT2                 not null,
+   ENTREGA_ID           SERIAL                 not null,
+   CARRO_ID             INTEGER                not null,
+   VENDA_ID             INTEGER                not null,
+   ENDERECO_ID          INTEGER                not null,
+   FUNCIONARIO_ID       INTEGER                not null,
    DATA_ENTREGA         DATE                 null,
    constraint PK_ENTREGA primary key (ENTREGA_ID),
    constraint FK_ENTREGA_R3_ENDERECO foreign key (ENDERECO_ID)
@@ -188,10 +174,10 @@ create table ENTREGA (
 /* Table: FORNECEDOR                                            */
 /*==============================================================*/
 create table FORNECEDOR (
-   FORNECEDOR_ID        INT2                not null,
-   NOME_FORN            VARCHAR(25)          null,
-   CNPJ_FORNECEDOR      VARCHAR(14)          not null,
-   EMAIL_FORNECEDOR     VARCHAR(25)          not null,
+   FORNECEDOR_ID        SERIAL                not null,
+   NOME_FORN            VARCHAR(50)          null,
+   CNPJ_FORNECEDOR      VARCHAR(50)          not null,
+   EMAIL_FORNECEDOR     VARCHAR(50)          not null,
    constraint PK_FORNECEDOR primary key (FORNECEDOR_ID)
 );
 
@@ -199,9 +185,9 @@ create table FORNECEDOR (
 /* Table: FORNECIMENTO                                          */
 /*==============================================================*/
 create table FORNECIMENTO (
-   FORNECIMENTO_ID      INT2                not null,
-   FRANQUIA_ID          INT2                 not null,
-   FORNECEDOR_ID        INT2                 not null,
+   FORNECIMENTO_ID      SERIAL                not null,
+   FRANQUIA_ID          INTEGER                 not null,
+   FORNECEDOR_ID        INTEGER                 not null,
    DATA_FORN            DATE                 null,
    constraint PK_FORNECIMENTO primary key (FORNECIMENTO_ID),
    constraint FK_FORNECIM_R6_FORNECED foreign key (FORNECEDOR_ID)
@@ -216,10 +202,10 @@ create table FORNECIMENTO (
 /* Table: PROPRIETARIO                                          */
 /*==============================================================*/
 create table PROPRIETARIO (
-   PROP_ID              INT2                not null,
-   CPF_PROP             VARCHAR(11)          null,
-   NOME_PROP            VARCHAR(25)          null,
-   SOBRENOME_PROP       VARCHAR(25)          null,
+   PROP_ID              SERIAL                not null,
+   CPF_PROP             VARCHAR(50)          null,
+   NOME_PROP            VARCHAR(50)          null,
+   SOBRENOME_PROP       VARCHAR(50)          null,
    constraint PK_PROPRIETARIO primary key (PROP_ID)
 );
 
@@ -227,9 +213,9 @@ create table PROPRIETARIO (
 /* Table: LOJA_TERCEIRA                                         */
 /*==============================================================*/
 create table LOJA_TERCEIRA (
-   LOJA_ID              INT2                 not null,
-   FRANQUIA_ID          INT2                 not null,
-   PROP_ID              INT2                 null,
+   LOJA_ID              SERIAL                 not null,
+   FRANQUIA_ID          INTEGER                 not null,
+   PROP_ID              INTEGER                 null,
    SALAO                INT2                 not null,
    constraint PK_LOJA_TERCEIRA primary key (LOJA_ID),
    constraint FK_LOJA_TER_R16_PROPRIET foreign key (PROP_ID)
@@ -244,8 +230,8 @@ create table LOJA_TERCEIRA (
 /* Table: SECAO                                                 */
 /*==============================================================*/
 create table SECAO (
-   SECAO_ID             INT2                 not null,
-   NOME_SEC             VARCHAR(25)          null,
+   SECAO_ID             SERIAL                 not null,
+   NOME_SEC             VARCHAR(50)          null,
    constraint PK_SECAO primary key (SECAO_ID)
 );
 
@@ -253,14 +239,14 @@ create table SECAO (
 /* Table: PRODUTO                                               */
 /*==============================================================*/
 create table PRODUTO (
-   PRODUTO_ID           INT2                not null,
-   SECAO_ID             INT2                 not null,
-   FRANQUIA_ID          INT2                 not null,
-   FORNECIMENTO_ID      INT2                 not null,
-   VENDA_ID             INT2                 null,
+   PRODUTO_ID           SERIAL                not null,
+   SECAO_ID             INTEGER                 not null,
+   FRANQUIA_ID          INTEGER                 not null,
+   FORNECIMENTO_ID      INTEGER                 not null,
+   VENDA_ID             INTEGER                 null,
    MARCA_PRODUTO        VARCHAR(50)          not null,
    PRECO_UNITARIO       MONEY                not null,
-   NOME_PRODUTO         VARCHAR(25)          null,
+   NOME_PRODUTO         VARCHAR(50)          null,
    constraint PK_PRODUTO primary key (PRODUTO_ID),
    constraint FK_PRODUTO_R5_FRANQUIA foreign key (FRANQUIA_ID)
       references FRANQUIA (FRANQUIA_ID)
@@ -280,9 +266,9 @@ create table PRODUTO (
 /* Table: PROMOCAO                                              */
 /*==============================================================*/
 create table PROMOCAO (
-   PORCENTAGEM          INT2                 not null,
-   PROMOCAO_ID          INT2                not null,
-   SECAO_ID             INT2                 not null,
+   PORCENTAGEM          SERIAL                 not null,
+   PROMOCAO_ID          INTEGER                not null,
+   SECAO_ID             INTEGER                 not null,
    constraint PK_PROMOCAO primary key (PROMOCAO_ID),
    constraint FK_PROMOCAO_RELATIONS_SECAO foreign key (SECAO_ID)
       references SECAO (SECAO_ID)
